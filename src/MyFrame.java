f37c0e5cf6e38fbd1c39a346bc1a20fb6ee7c267
Gursahil Singh <gursahil4u@gmail.com>
2024-06-25 22:40:38 +0100

    Added all components of header

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -1,23 +1,33 @@
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Image;
-
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
 
-public class MyFrame extends JFrame{
-   
+
+
+
+public class MyFrame extends JFrame implements ActionListener{
+    JButton algorithmSelector;
+    JPopupMenu menu;
+
+
     MyFrame(){
+        // RESET BUTTON
         ImageIcon resetArrayIcon = new ImageIcon("assests\\Reset.png");
         Image image = resetArrayIcon.getImage();
         Image resizedImage = image.getScaledInstance(15, 15, Image.SCALE_SMOOTH);
         ImageIcon resizedIcon = new ImageIcon(resizedImage);
 
         JButton resetArray = new JButton();
-        resetArray.setBounds(20, 10, 100, 30);
+        resetArray.setBounds(25, 10, 100, 30);
         resetArray.setText("Reset");
         resetArray.setFocusable(false);
         resetArray.setIcon(resizedIcon);
@@ -28,18 +38,62 @@ public class MyFrame extends JFrame{
         resetArray.setBackground(Color.LIGHT_GRAY);
         resetArray.setBorder(BorderFactory.createRaisedBevelBorder());
 
+        //Algorithm Chooser
+        menu = new JPopupMenu("Algorithm");
 
+        JMenuItem item1 = new JMenuItem("Insertion Sort");
+        JMenuItem item2 = new JMenuItem("Selection Sort");
+        JMenuItem item3 = new JMenuItem("Merge Sort");
+        menu.add(item1);
+        menu.add(item2);
+        menu.add(item3);
+        menu.setPopupSize(200,100);
+        menu.setFont(new Font("Comic Sans", Font.BOLD, 15));
+        
+        algorithmSelector = new JButton();
+        algorithmSelector.setBounds(375, 10, 200, 30);
+        algorithmSelector.setText("Select Algorithm");
+        algorithmSelector.setFocusable(false);
+        algorithmSelector.setFont(new Font("Comic Sans", Font.BOLD, 12));
+        algorithmSelector.setBackground(Color.LIGHT_GRAY);
+        algorithmSelector.setBorder(BorderFactory.createRaisedBevelBorder());
+        algorithmSelector.addActionListener(this);
+       
+
+        // Run Button
+        ImageIcon runProgramIcon = new ImageIcon("assests\\run.png");
+        Image runImage = runProgramIcon.getImage();
+        Image resizedRunImage = runImage.getScaledInstance(15, 15, Image.SCALE_SMOOTH);
+        ImageIcon resizedRunIcon = new ImageIcon(resizedRunImage);
+        JButton runProgram = new JButton();
+        runProgram.setBounds(150, 10, 200, 30);
+        runProgram.setText("Run: " + "Insertion Sort");
+        runProgram.setFocusable(false);
+        runProgram.setIcon(resizedRunIcon);
+        runProgram.setVerticalTextPosition(JButton.BOTTOM);
+        runProgram.setHorizontalTextPosition(JButton.CENTER);
+        runProgram.setIconTextGap(-4);
+        runProgram.setFont(new Font("Comic Sans", Font.BOLD, 12));
+        runProgram.setBackground(Color.LIGHT_GRAY);
+        runProgram.setBorder(BorderFactory.createRaisedBevelBorder());
+
+
+        // HEADER 
         JPanel header = new JPanel();
         header.setBackground(Color.DARK_GRAY);
         header.setBounds(0, 0, 600, 50);
         header.setLayout(null);
         header.add(resetArray);
-        
+        header.add(algorithmSelector);
+        header.add(runProgram);
+
+
+        // FRAME
         this.setTitle("Algorithm Visiualizer");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(null);
         this.setResizable(false);
-        this.setSize(600, 500);
+        this.setSize(614, 500);
         this.setVisible(true);
         this.add(header);
 
@@ -49,5 +103,10 @@ public class MyFrame extends JFrame{
         this.getContentPane().setBackground(Color.black);
 
     }
-        
+        @Override
+        public void actionPerformed(ActionEvent e){
+            if(e.getSource()==algorithmSelector){
+                menu.show(algorithmSelector, 0, 0);
+            }
+        }
 }
